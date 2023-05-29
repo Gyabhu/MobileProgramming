24cd2cf554084bf4e8869181f4f4a4faf1e53662
Gyabhu <gabriel.moktan777@gmail.com>
2023-05-29 07:50:40 +0545

    Simple calculator start...

---
+++ app/src/main/java/com/example/unitfive/Calculator.java
@@ -0,0 +1,7 @@
+package com.example.unitfive;
+
+import androidx.fragment.app.Fragment;
+
+public class Calculator extends Fragment {
+
+}
--- app/src/main/java/com/example/unitfive/FragmentOneTwo.java
+++ app/src/main/java/com/example/unitfive/FragmentOneTwo.java
@@ -2,11 +2,17 @@ package com.example.unitfive;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class FragmentOneTwo extends Activity {
     @Override
     protected void onCreate(Bundle b){
-        super.onCreate(b);
-        setContentView(R.layout.fragment_one_two);
+        try {
+            super.onCreate(b);
+            setContentView(R.layout.fragment_one_two);
+        }catch (Exception e){
+            Log.d("Error", "onCreate of main activity");
+            throw e;
+        }
     }
 }
